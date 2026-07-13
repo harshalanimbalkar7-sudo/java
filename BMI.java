71f6d018f7e387a31e54f7c08858d22e3039e4d9
harshalanimbalkar7-sudo <harshalanimbalkar7@gmail.com>
2026-07-13 18:16:58 +0530

    Add files via upload

--- BMI.java
+++ BMI.java
@@ -58,3 +58,24 @@ public class ArithmeticOperations {
         sc.close();
     }
 }
+
+import java.util.Scanner;
+
+public class AddNumbers {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+
+        int sum = a + b;
+
+        System.out.println("Sum = " + sum);
+
+        sc.close();
+    }
+}
