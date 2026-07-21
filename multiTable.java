e7e7e4e3d0ee22fb811adf68a3dad97ce6838e28
harshalanimbalkar7-sudo <harshalanimbalkar7@gmail.com>
2026-07-21 20:02:26 +0530

    Add files via upload

--- multiTable.java
+++ multiTable.java
@@ -39,3 +39,24 @@ public class CountrySort {
         sc.close();
     }
 }
+
+public class PrimeNumbers {
+    public static void main(String[] args) {
+
+        System.out.println("Prime numbers from 1 to 100 are:");
+
+        for (int i = 2; i <= 100; i++) {
+            int count = 0;
+
+            for (int j = 1; j <= i; j++) {
+                if (i % j == 0) {
+                    count++;
+                }
+            }
+
+            if (count == 2) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
