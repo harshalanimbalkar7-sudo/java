d32847b4f95ac12fc94f743c2ee95dd85fbc5078
harshalanimbalkar7-sudo <harshalanimbalkar7@gmail.com>
2026-07-21 20:01:12 +0530

    Add files via upload

--- multiTable.java
+++ multiTable.java
@@ -8,3 +8,34 @@ public class multiTable {
     }
 }
 }
+
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class CountrySort {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of countries: ");
+        int n = sc.nextInt();
+        sc.nextLine();
+
+        String country[] = new String[n];
+
+        System.out.println("Enter country names:");
+        for (int i = 0; i < n; i++) {
+            country[i] = sc.nextLine();
+        }
+
+        Arrays.sort(country);
+
+        System.out.println("Countries in Descending Order:");
+        for (int i = n - 1; i >= 0; i--) {
+            System.out.println(country[i]);
+        }
+
+        sc.close();
+    }
+}
